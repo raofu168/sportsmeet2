d262e53c960102ce35f071ca9381c8fa2cbd9242
raofu168 <raofu168@gmail.com>
2014-08-17 03:09:49 -0700

    reviews link to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140817095209) do
+ActiveRecord::Schema.define(version: 20140817100237) do
 
   create_table "locations", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140817095209) do
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
